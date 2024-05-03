2e131e10860978ef4d91c735988ab63d6c71c3fa
frandesou <fender.fds@gmail.com>
2024-05-02 23:36:03 -0300

    Se agrega index con bootstrap

--- FedeteriAPI/obj/Debug/net8.0/FedeteriAPI.AssemblyInfo.cs
+++ FedeteriAPI/obj/Debug/net8.0/FedeteriAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FedeteriAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ad347c9771485cafdcb086fdfcb2b8c9356465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4481e12e73613e65bb6091b242ee196ed198982e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FedeteriAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FedeteriAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
