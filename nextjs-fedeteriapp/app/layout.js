5a388a526b3d7230cc708e1a4afc5d84fb7c545b
Weby <nicoyurec@gmail.com>
2024-05-05 20:35:26 -0300

    Hecho el login

--- nextjs-fedeteriapp/app/layout.js
+++ nextjs-fedeteriapp/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ContextProvider from "@/components/ContextProvider/ContextProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <head>
         <link rel="icon" href="favicon.ico" sizes="any" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" precedence="default" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link>
@@ -43,7 +44,9 @@ export default function RootLayout({ children }) {
           </div>
         </nav>
         <main className="d-flex justify-content-center align-items-center min-vw-100 min-vh-100">
-          {children}
+          <ContextProvider>
+            {children}
+          </ContextProvider>
         </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
       </body>
