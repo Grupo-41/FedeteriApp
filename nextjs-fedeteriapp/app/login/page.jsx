5582dbcd1236dcd0076f7ee6ef121eac04b894f7
Weby <nicoyurec@gmail.com>
2024-05-07 19:10:09 -0300

    Agrego toaster y comprobación de login

--- nextjs-fedeteriapp/app/login/page.jsx
+++ nextjs-fedeteriapp/app/login/page.jsx
@@ -1,12 +1,21 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useLocalStorage } from 'react-use';
+import toast from 'react-hot-toast';
+
 
 const Page = () => {
     const refDNI = useRef();
     const refPass = useRef();
+    const [error, setError] = useState(false)
     const [user, setUser, removeUser] = useLocalStorage('user', null);
     const [email, setEmail, removeEmail] = useLocalStorage('email-recovery', '');
+
+    function keyDown(e){
+      if (e.key === 'Enter' || e.keyCode === 13) {
+        clickLogin();
+      }
+    } 
 
     function clickLogin(){
         fetch('http://localhost:5000/api/Usuarios/login?' + new URLSearchParams({
@@ -14,11 +23,9 @@ const Page = () => {
             contrasena: refPass.current.value,
         }))
         .then(data => {
-          console.log(data)
           return data.json()
         })
         .then(data => {
-          console.log(data)
             if(data.status !== 400){
                 if(data.esAdmin){
                     data = {...data, validado: false}
@@ -32,7 +39,14 @@ const Page = () => {
 
                 removeEmail();
             }
+            else{
+              toast.error('Credenciales inválidas.')
+            }
         })
+        .catch(() => {
+          toast.error('Credenciales inválidas.')
+        })
+
     }
     
   return (
@@ -44,13 +58,13 @@ const Page = () => {
             </div>
             <div className="mb-4">
               <label htmlFor="contra" className="form-label">Contraseña</label>
-              <input ref={refPass} type="password" placeholder="Ingrese su contraseña" className="form-control border border-dark" id="contra"/>
+              <input onKeyDown={(e) => keyDown(e)} ref={refPass} type="password" placeholder="Ingrese su contraseña" className="form-control border border-dark" id="contra"/>
             </div>
             <div className='d-flex flex-row justify-content-between align-items-center'>
               <a href="/recovery-password">Olvidé mi contraseña</a>
               <button onClick={clickLogin} type="button" style={{backgroundColor: '#e7ab12 ', float: 'right'}} className="btn">Iniciar sesión</button>
             </div>
-          </form>
+        </form>
     </div>
   )
 }
