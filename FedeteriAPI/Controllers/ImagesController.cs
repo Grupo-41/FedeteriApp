b2cc947279ba60f84fface0f0e92acb70340f323
Weby <nicoyurec@gmail.com>
2024-05-08 16:54:56 -0300

    Cambios en la API para aceptar imágenes de artículos y devolverlas. Además se eliminó el campo nombre de los artículos

---
+++ FedeteriAPI/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FedeteriAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpGet("{imageName}")]
+        public IActionResult Get(string imageName)
+        {
+            Byte[] b = System.IO.File.ReadAllBytes($@"Images/{imageName}");   // You can use your own method over here.         
+            return File(b, "image/jpeg");
+        }
+    }
+}
