cc8807aa1e7c54a462ec1097eecfc7299e73c102
Weby <nicoyurec@gmail.com>
2024-05-14 14:07:37 -0300

    Cambios en la página principal para mejor visualización de los artículos

--- nextjs-fedeteriapp/app/page.js
+++ nextjs-fedeteriapp/app/page.js
@@ -16,18 +16,18 @@ export default function Home() {
 
   return (
     <>
-      <div className="d-flex flex-column gap-5 align-items-center">
+      <div className="mt-5 d-flex flex-column gap-5 align-items-center justify-content-center">
         <div className="mb-5">
           { articulos.length > 0 &&
             <>
               <h2 className="mb-3">Publicaciones</h2>
-              <div style={{minWidth: '400px', maxWidth: '58rem', paddingRight: '17px', maxHeight: '35vh', overflow: 'auto'}} className="d-flex flex-row justify-content-center flex-wrap gap-3 align-self-center">
+              <div style={{minWidth: '400px', maxWidth: '58rem', paddingRight: '17px', maxHeight: '44vh', overflow: 'auto'}} className="d-flex flex-row justify-content-center flex-wrap gap-3 align-self-center">
                 {articulos.map(x => <Publicacion key={x.id} item={x} />)}
               </div>
             </>
           }
         </div>
-        <div style={articulos.length > 0 ? {position:'relative', bottom: '-100px', zoom: 0.75} : {}}>
+        <div style={articulos.length > 0 ? {zoom: 0.75} : {}}>
           <Image width={400} src={Logo} />
           <h3 className="mt-5 text-center">Work in progress...</h3>
         </div>
