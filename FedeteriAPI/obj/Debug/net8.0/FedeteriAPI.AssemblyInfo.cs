9a9df16361aaad6e56642dddf2ffe0fd5d4b7893
Weby <nicoyurec@gmail.com>
2024-05-27 01:05:47 -0300

    Bugs del hardcodeo arreglados

--- FedeteriAPI/obj/Debug/net8.0/FedeteriAPI.AssemblyInfo.cs
+++ FedeteriAPI/obj/Debug/net8.0/FedeteriAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FedeteriAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d23ea290ea53ffde60c3490206687cab17207ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f3a98c8e3d84c844cccf85e44e7062f7b27072")]
 [assembly: System.Reflection.AssemblyProductAttribute("FedeteriAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FedeteriAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
