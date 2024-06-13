a3e6d4351ecf1ffdb3ff1fab2f3685f3109c07c3
Weby <nicoyurec@gmail.com>
2024-06-13 14:52:32 -0300

    Estados dinámicos en la búsqueda de artículos. Ahora también se ocultan los filtros cuando no hay artículos para mostrar/filtrar

--- nextjs-fedeteriapp/app/buscar/[query]/page.jsx
+++ nextjs-fedeteriapp/app/buscar/[query]/page.jsx
@@ -48,7 +48,7 @@ const Page = ({ params }) => {
     }, [])
 
     return (
-        <div style={{ marginLeft: '200px', marginTop: '100px', marginBottom: '25px' }} className="d-flex flex-column align-items-center justify-content-center w-100">
+        <div style={{ marginLeft: articulos && articulos.length > 0 ? '200px' : '0', marginTop: '100px', marginBottom: '25px' }} className="d-flex flex-column align-items-center justify-content-center w-100">
             <h1>Resultados de la búsqueda: &quot;{query}&quot;</h1>
             <div style={{ minWidth: '400px', maxWidth: '58rem' }} className="mt-4 d-flex flex-row justify-content-center flex-wrap gap-4 align-self-center">
                 {articulos && articulos.length > 0 ?
@@ -60,69 +60,55 @@ const Page = ({ params }) => {
                     <p>No hay artículos para mostrar</p>
                 }
             </div>
-            <div className="position-absolute shadow d-flex flex-column justify-content-center start-0 ps-5 gap-5 top-0 bg-white border-end border-secondary-subtle" style={{ minWidth: '260px', paddingTop: '75px', height: '100vh' }}>
-                <div>
-                    <h3>Marca</h3>
-                    {
-                        articulos.map(x => x.marca).filter(onlyUnique).sort().map((y, index) => {
-                            return (
-                                <div key={index} className="form-check">
-                                    <input className="form-check-input" onInput={(e) => {
-                                        if(e.target.checked){
-                                            addFilter(y, filtrosMarca, setFiltrosMarca)
-                                        }
-                                        else{
-                                            removeFilter(y, filtrosMarca, setFiltrosMarca)
-                                        }
-                                    }} type="checkbox" value={y} id={`checkMarca${index}`} />
-                                    <label className="form-check-label" htmlFor={`checkMarca${index}`}>
-                                        {y}
-                                    </label>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                <div>
-                    <h3>Estado</h3>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" onInput={(e) => {
-                                        if(e.target.checked){
-                                            addFilter("Nuevo", filtrosEstado, setFiltrosEstado)
-                                        }
-                                        else{
-                                            removeFilter("Nuevo", filtrosEstado, setFiltrosEstado)
-                                        }}} value="" id="checkStatus1" />
-                        <label className="form-check-label" htmlFor="checkStatus1">
-                            Nuevo
-                        </label>
+            {
+                articulos && articulos.length > 0 &&
+                <div className="position-absolute shadow d-flex flex-column justify-content-center start-0 ps-5 gap-5 top-0 bg-white border-end border-secondary-subtle" style={{ minWidth: '260px', paddingTop: '75px', height: '100vh' }}>
+                    <div>
+                        <h3>Marca</h3>
+                        {
+                            articulos.map(x => x.marca).filter(onlyUnique).sort().map((y, index) => {
+                                return (
+                                    <div key={index} className="form-check">
+                                        <input className="form-check-input" onInput={(e) => {
+                                            if(e.target.checked){
+                                                addFilter(y, filtrosMarca, setFiltrosMarca)
+                                            }
+                                            else{
+                                                removeFilter(y, filtrosMarca, setFiltrosMarca)
+                                            }
+                                        }} type="checkbox" value={y} id={`checkMarca${index}`} />
+                                        <label className="form-check-label" htmlFor={`checkMarca${index}`}>
+                                            {y}
+                                        </label>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" onInput={(e) => {
-                                        if(e.target.checked){
-                                            addFilter("Usado", filtrosEstado, setFiltrosEstado)
-                                        }
-                                        else{
-                                            removeFilter("Usado", filtrosEstado, setFiltrosEstado)
-                                        }}} value="" id="checkStatus2" />
-                        <label className="form-check-label" htmlFor="checkStatus2">
-                            Usado
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" onInput={(e) => {
-                                        if(e.target.checked){
-                                            addFilter("Reacondicionado", filtrosEstado, setFiltrosEstado)
-                                        }
-                                        else{
-                                            removeFilter("Reacondicionado", filtrosEstado, setFiltrosEstado)
-                                        }}} value="" id="checkStatus3" />
-                        <label className="form-check-label" htmlFor="checkStatus3">
-                            Reacondicionado
-                        </label>
+                    <div>
+                        <h3>Estado</h3>
+                        {
+                            articulos.map(x => x.estado).filter(onlyUnique).sort().map((y, index) => {
+                                return (
+                                    <div key={index} className="form-check">
+                                        <input className="form-check-input" onInput={(e) => {
+                                            if(e.target.checked){
+                                                addFilter(y, filtrosEstado, setFiltrosEstado)
+                                            }
+                                            else{
+                                                removeFilter(y, filtrosEstado, setFiltrosEstado)
+                                            }
+                                        }} type="checkbox" value={y} id={`checkEstado${index}`} />
+                                        <label className="form-check-label" htmlFor={`checkEstado${index}`}>
+                                            {y}
+                                        </label>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
