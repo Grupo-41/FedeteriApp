78050dbf0fc629794c6b9e8cb34e57232ff4328a
frandesou <fender.fds@gmail.com>
2024-06-20 20:27:30 -0300

    Agregada funcionalidad para validar cupones

---
+++ nextjs-fedeteriapp/app/validar-cupon/page.jsx
@@ -0,0 +1,53 @@
+"use client"
+import React, { useRef, useState } from 'react'
+import toast from 'react-hot-toast'
+
+const Page = () => {
+    const cuponRef = useRef();
+    const [cupon, setCupon] = useState(null);
+
+    function validateCode() {
+        const codigo = cuponRef.current.value;
+
+        if (!codigo) {
+            toast.error("Debe ingresar un código.")
+            return;
+        }
+
+        const URL = `http://localhost:5000/api/validar-cupon/${codigo}`;
+
+        fetch(URL)
+            .then(data => {
+                if (data.status === 200)
+                    return data.json()
+                return null
+            })
+            .then(data => {
+                console.log(data)
+                setCupon(data)
+                if (data !== null) {
+                    toast.success("El cupón fue validado correctamente")
+                }
+                else
+                    toast.error("El código es incorrecto.")
+            })
+
+    }
+
+    return (
+        <div className="mt-5 d-flex justify-content-center w-100">
+            <form style={{ minWidth: '400px', background: 'white' }} className="border rounded p-4 w-25 align-self-center">
+                <div className='mb-3'>
+                    <h3 className='mb-3'>Validador de cupones</h3>
+                    <input ref={cuponRef} placeholder='Ingrese el código' type="text" className="form-control" id="cupon" />
+                </div>
+                {cupon &&
+                    <p className='alert alert-success'>El cupón canjeado tiene un descuento de: ${cupon.valor}</p>
+                }
+                <button onClick={validateCode} type="button" className="btn float-end" style={{ background: '#e7ab12' }}>Validar cupón</button>
+            </form>
+        </div>
+    )
+}
+
+export default Page
--- nextjs-fedeteriapp/components/Navbar/Navbar.jsx
+++ nextjs-fedeteriapp/components/Navbar/Navbar.jsx
@@ -10,118 +10,123 @@ const Navbar = () => {
     const [loading, setLoading] = useState(true);
     const refInput = useRef();
 
-    function closeSession(){
+    function closeSession() {
         removeUser();
         window.location.href = '/';
     }
 
-    function inputKeyDown(e){
+    function inputKeyDown(e) {
         if (e.key === 'Enter' || e.keyCode === 13) {
             e.preventDefault();
             onClickSearch();
         }
     }
 
-    function onClickSearch(){
-        if(refInput.current.value && refInput.current.value.length > 0){
-            if(typeof(window) !== 'undefined')
+    function onClickSearch() {
+        if (refInput.current.value && refInput.current.value.length > 0) {
+            if (typeof (window) !== 'undefined')
                 window.location.href = '/buscar/' + refInput.current.value;
         }
     }
 
     useEffect(() => {
         setLoading(false);
-
-        const liElements = document.querySelectorAll(".navbar-nav li");
-
-        liElements.forEach(x => x)
-
     }, [])
 
-  return (
-    <>
-        <nav className="navbar fixed-top navbar-expand-lg shadow border-bottom border-black" style={{background: '#e7ab12 '}}>
-            <div className="container-fluid">
-                <a className="navbar-brand me-3 ms-2" href="/"><Image src={FedeteriaLogo} height="30" alt="FedeteriApp"/></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto gap-1 mb-2 mb-lg-0">
-                        { !user && !loading && 
-                        <>
-                            <li key={"registrarme"} className="nav-item">
-                                <a className="nav-link" href="/signup">Registrarme</a>
-                            </li>
-                            <li key={"login"} className="nav-item">
-                                <a className="nav-link" href="/login">Iniciar sesión</a>
-                            </li>
-                        </>
-                        }
-                        { user &&
-                        <>
-                            <li key={"profile"} className="nav-item">
-                                <a className="nav-link" href="/profile">Mi perfil</a>
-                            </li>
-                            {
-                                !user.esAdmin &&
+    return (
+        <>
+            <nav className="navbar fixed-top navbar-expand-lg shadow border-bottom border-black" style={{ background: '#e7ab12 ' }}>
+                <div className="container-fluid">
+                    <a className="navbar-brand me-3 ms-2" href="/"><Image src={FedeteriaLogo} height="30" alt="FedeteriApp" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto gap-1 mb-2 mb-lg-0">
+                            {!user && !loading &&
                                 <>
-                                    <li key={"publicar"} className="nav-item">
-                                        <a className="nav-link" href="/publicar">Publicar artículo</a>
+                                    <li key={"registrarme"} className="nav-item">
+                                        <a className="nav-link" href="/signup">Registrarme</a>
                                     </li>
-                                    <li key={"publicaciones"} className="nav-item">
-                                        <a className="nav-link" href="/mis-publicaciones">Mis publicaciones</a>
-                                    </li>
-                                    <li key={"propuestas"} className="nav-item">
-                                        <a className="nav-link" href="/propuestas">Mis propuestas</a>
-                                    </li>
-                                    <li key={"mis-trueques"} className="nav-item">
-                                        <a className="nav-link" href="/mis-trueques">Mis trueques</a>
+                                    <li key={"login"} className="nav-item">
+                                        <a className="nav-link" href="/login">Iniciar sesión</a>
                                     </li>
                                 </>
                             }
-                            <li key={"sucursales"} className="nav-item">
-                                <a className="nav-link" href="/sucursales">Ver sucursales</a>
-                            </li>
-                        </>
-                        }
-                        { user && user.esAdmin &&
-                        <>
-                            <li key={"registrarEmpleado"} className="nav-item">
-                                <a className="nav-link" href="/registrar-empleado">Registrar empleado</a>
-                            </li>
-                        </>
-                        }
-                        { user && (user.esEmpleado || user.esAdmin) &&
+                            {user &&
+                                <>
+                                    <li key={"profile"} className="nav-item">
+                                        <a className="nav-link" href="/profile">Mi perfil</a>
+                                    </li>
+                                    {
+                                        !user.esAdmin &&
+                                        <>
+                                            <li key={"publicar"} className="nav-item">
+                                                <a className="nav-link" href="/publicar">Publicar artículo</a>
+                                            </li>
+                                            <li key={"publicaciones"} className="nav-item">
+                                                <a className="nav-link" href="/mis-publicaciones">Mis publicaciones</a>
+                                            </li>
+                                            <li key={"propuestas"} className="nav-item">
+                                                <a className="nav-link" href="/propuestas">Mis propuestas</a>
+                                            </li>
+                                            <li key={"mis-trueques"} className="nav-item">
+                                                <a className="nav-link" href="/mis-trueques">Mis trueques</a>
+                                            </li>
+                                        </>
+                                    }
+                                    <li key={"sucursales"} className="nav-item">
+                                        <a className="nav-link" href="/sucursales">Ver sucursales</a>
+                                    </li>
+                                </>
+                            }
+                            {user && (user.esEmpleado || user.esAdmin) &&
+                                <>
+                                    <li class="nav-item dropdown">
+                                        <button class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" style={{ background: '#e7ab12 ' }}>
+                                            Administración
+                                        </button>
+                                        <ul class="dropdown-menu" style={{ background: '#e7ab12 ' }}>
+                                            <li key={"tasarArticulo"} className="nav-item">
+                                                <a className="nav-link" href="/tasar-articulos">Tasar artículos</a>
+                                            </li>
+                                            <li key={"truequesPendientes"} className="nav-item">
+                                                <a className="nav-link" href="/trueques-pendientes">Trueques pendientes</a>
+                                            </li>
+                                            <li key={"estadisticas"} className="nav-item">
+                                                <a className="nav-link" href="/estadisticas">Ver estadísticas</a>
+                                            </li>
+                                            <li key={"validarCupon"} className="nav-item">
+                                                <a className="nav-link" href="/validar-cupon">Validar cupón</a>
+                                            </li>
+                                            {user && user.esAdmin &&
+                                                <>
+                                                    <li key={"registrarEmpleado"} className="nav-item">
+                                                        <a className="nav-link" href="/registrar-empleado">Registrar empleado</a>
+                                                    </li>
+                                                </>
+                                            }
+                                        </ul>
+                                    </li>
+                                </>
+                            }
+                        </ul>
+                        {user &&
                             <>
-                                <li key={"tasarArticulo"} className="nav-item">
-                                    <a className="nav-link" href="/tasar-articulos">Tasar artículos</a>
-                                </li>
-                                <li key={"truequesPendientes"} className="nav-item">
-                                    <a className="nav-link" href="/trueques-pendientes">Trueques pendientes</a>
-                                </li>
-                                <li key={"estadisticas"} className="nav-item">
-                                    <a className="nav-link" href="/estadisticas">Ver estadísticas</a>
-                                </li>
+                                <form className="d-flex position-relative" style={{ width: '300px' }} role="search">
+                                    <input ref={refInput} onKeyDown={e => inputKeyDown(e)} className="w-100 form-control me-2" type="search" placeholder="Buscar por artículo o marca..." aria-label="Search" />
+                                    <button type='button' onClick={onClickSearch} style={{ marginTop: '5px', marginRight: '18px' }} className='position-absolute end-0'><BiSearch size={20} fill='gray' /></button>
+                                </form>
+                                <button onClick={closeSession} className="ms-2 btn btn-outline-secondary">Cerrar sesión</button>
                             </>
                         }
-                    </ul>
-                    { user &&
-                        <>
-                            <form className="d-flex position-relative" style={{width: '300px'}} role="search">
-                                <input ref={refInput} onKeyDown={e => inputKeyDown(e)} className="w-100 form-control me-2" type="search" placeholder="Buscar por artículo o marca..." aria-label="Search" />
-                                <button type='button' onClick={onClickSearch} style={{marginTop: '5px', marginRight: '18px'}} className='position-absolute end-0'><BiSearch size={20} fill='gray' /></button>
-                            </form>
-                            <button onClick={closeSession} className="ms-2 btn btn-outline-secondary">Cerrar sesión</button>
-                        </>
-                    }
+                    </div>
                 </div>
-            </div>
             </nav>
-            
-    </>
-  )
+
+        </>
+    )
 }
 
 export default Navbar
