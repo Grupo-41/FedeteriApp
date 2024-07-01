3974beb8e267a1259b6714c1ac04b89a57317b9d
Weby <nicoyurec@gmail.com>
2024-06-30 22:34:09 -0300

    Avances en estadísticas

--- nextjs-fedeteriapp/app/estadisticas/page.jsx
+++ nextjs-fedeteriapp/app/estadisticas/page.jsx
@@ -10,18 +10,21 @@ const Page = () => {
     const refSucursal = useRef();
     const [sucursales, setSucursales] = useState([]);
     const [estadisticaVentas, setEstadisticaVentas] = useState([]);
-    const [ventasPorSucursal, setVentasPorSucursal] = useState([]);
+    const [estadisticaSucursales, setEstadisticaSucursales] = useState([]);
 
     const dataVentas = estadisticaVentas.map(x => {
         return {
-            name: x.sucursal.nombre,
+            name: x.sucursal.nombre + ` (${x.ventas.length})`,
             value: x.ventas.length
         }
     })
 
+
     const dataMontos = estadisticaVentas.map(x => {
+        const monto = x.montoTotal > 1000 ? ` ($${(x.montoTotal / 1000).toFixed(1)}k)`: `($${x.montoTotal})`
+
         return {
-            name: x.sucursal.nombre,
+            name: x.sucursal.nombre + monto,
             value: x.montoTotal
         }
     })
@@ -32,14 +35,19 @@ const Page = () => {
             subtext: 'Por sucursal',
             left: 'center'
         },
-        tooltip: {
-            trigger: 'item'
+        legend: {
+            orient: 'vertical',
+            left: 'left'
         },
         series: [
             {
                 name: 'Ventas',
                 type: 'pie',
                 radius: '50%',
+                label: {
+                    show: false,
+                    overflow: "show"
+                },
                 data: [
                     ...dataVentas
                 ]
@@ -56,11 +64,19 @@ const Page = () => {
         tooltip: {
             trigger: 'item'
         },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
         series: [
             {
                 name: 'Ventas',
                 type: 'pie',
                 radius: '50%',
+                label: {
+                    show: false,
+                    overflow: "show"
+                },
                 data: [
                     ...dataMontos
                 ]
@@ -82,12 +98,42 @@ const Page = () => {
             .then(data => {
                 setEstadisticaVentas(data)
             })
+
+        URL = 'http://localhost:5000/api/Calificaciones/sucursales'
+
+        fetch(URL)
+            .then(data => data.json())
+            .then(data => setEstadisticaSucursales(data));
     }, [])
 
     return (
         <>
-            <ReactECharts style={{ height: '50vh', width: '50vh' }} option={optionVentas} />
-            <ReactECharts style={{ height: '50vh', width: '50vh' }} option={optionMontoPorVentas} />
+            <div className='d-flex flex-row gap-4' style={{ marginTop: '75px' }}>
+                <div className='d-flex flex-column gap-3 ps-4 pt-4' style={{ background: 'white', borderRadius: '5px', width: '530px', height: '75vh' }}>
+                    <h2 className='mb-4'>Ventas</h2>
+                    <div className='ms-3'>
+                        <ReactECharts style={{ height: '35vh', width: '675px' }} option={optionVentas} />
+                        <ReactECharts style={{ height: '35vh', width: '675px', marginTop: '-25px' }} option={optionMontoPorVentas} />
+                    </div>
+                </div>
+                <div>
+                    <div className='d-flex flex-column gap-3 p-4' style={{ background: 'white', borderRadius: '5px', width: '400px' }}>
+                        <h2 className='mb-2'>Ferreterias</h2>
+                        <ul className="list-group">
+                            {estadisticaSucursales.map(x => {
+                                return (
+                                    <li className="list-group-item d-flex justify-content-between align-items-start">
+                                        {x.sucursal.nombre} - {x.votantes} votos
+                                        <span className="badge rounded-pill" style={{background: '#509'}}>{x.rating} ⭐</span>
+                                    </li>
+                                );
+                            })}
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
             <div className="position-absolute shadow d-flex flex-column justify-content-center start-0 gap-5 ps-5 top-0 bg-white border-end border-secondary-subtle" style={{ minWidth: '330px', paddingTop: '75px', height: '100vh' }}>
                 <div>
                     <h3 className='mb-3'>Filtrar por fecha</h3>
@@ -104,18 +150,6 @@ const Page = () => {
                                 <DatePicker minDate={dayjs('2024-01-01')} maxDate={dayjs()} defaultValue={dayjs()} />
                             </LocalizationProvider>
                         </div>
-                    </div>
-                </div>
-                <div>
-                    <h3 className='mb-3'>Filtrar por sucursal</h3>
-                    <div className="mb-3 w-75">
-                        <label htmlFor="sucursal-choice" className="form-label">Sucursal:</label>
-                        <select id="sucursal-list" ref={refSucursal} className='form-control form-select' style={{ borderColor: '#C4C4C4' }} required >
-                            <option value={-1}>Todas las sucursales</option>
-                            {sucursales.map(x =>
-                                <option key={x.id} value={x.id}>{x.nombre}</option>
-                            )}
-                        </select>
                     </div>
                 </div>
             </div>
